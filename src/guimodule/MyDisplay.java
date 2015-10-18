67cae551b8e01d457bb4e4b3421a8c1feee7bc6e
Mohammed Ali <moh_fcis@yahoo.com>
2015-10-18 04:54:36 +0200

    Module 2 add MyDisplay

---
+++ src/guimodule/MyDisplay.java
@@ -0,0 +1,27 @@
+package guimodule;
+
+import processing.core.PApplet;
+
+public class MyDisplay extends PApplet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public void setup() {
+		size(400,400);
+		background(200, 200, 200);
+	}
+
+
+	public void draw() {
+		ellipse(200, 200, 390, 390);
+	}
+
+	
+	
+	
+	
+
+}
