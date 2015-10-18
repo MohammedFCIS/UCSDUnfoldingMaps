518ee642eabe9da1caa65be91c0433b8ef31b18f
Mohammed Ali <moh_fcis@yahoo.com>
2015-10-18 05:02:30 +0200

    Add Happy Face

--- src/guimodule/MyDisplay.java
+++ src/guimodule/MyDisplay.java
@@ -16,7 +16,12 @@ public class MyDisplay extends PApplet{
 
 
 	public void draw() {
-		ellipse(200, 200, 390, 390);
+		fill(255,255,0);//yellow
+		ellipse(200, 200, 390, 390);//the face
+		fill(0,0,0);//black
+		ellipse(120,130,50,70);//left eye		
+		ellipse(270,130,50,70);//right eye
+		arc(200,280,75,75,0,PI);//the smile :)
 	}
 
 	
